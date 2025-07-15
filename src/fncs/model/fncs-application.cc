6a5e2d947e9f4b329728fce3af6beb11707c7f89
tcod <91478141+tocods@users.noreply.github.com>
2025-07-15 17:00:31 +0800

    Revert "修复了回传计算仿真器的bug"
    This reverts commit 152db83738ee5e04f91e9c50df4083df261da40c.

--- src/fncs/model/fncs-application.cc
+++ src/fncs/model/fncs-application.cc
@@ -434,7 +434,6 @@ FncsApplication::HandleRead (Ptr<Socket> socket)
   NS_LOG_FUNCTION (this << socket);
   Ptr<Packet> packet;
   Address from;
-  std::string tosend = "";
   while ((packet = socket->RecvFrom (from)))
     {
       uint32_t size = packet->GetSize();
@@ -526,21 +525,12 @@ FncsApplication::HandleRead (Ptr<Socket> socket)
 		          << packet->GetUid () <<"'");
         }
       //fncs::publish(topic, value);
-      std::string value_no_zero = value;
-      value_no_zero.erase(std::remove(value_no_zero.begin(), value_no_zero.end(), '\0'), value_no_zero.end());
-      int s = value_no_zero.length();
-      NS_LOG_INFO ("FncsApplication::HandleRead: topic '" << topic << "' value '" << value << "' size " << s);
-      // if (s > 0 && value[s-1] == '\n') {
-      //   value.erase(s-1); // remove trailing newline
-      // }
-      tosend = tosend.append(value_no_zero).append("/");
-      
+       std::string v2send = value + "=sendend";
       NS_LOG_DEBUG ("FncsApplication::HandleRead: calling fncs::publish for topic '" << topic << "' value '" << value << "'");
-      NS_LOG_INFO ("FncsApplication::HandleRead: calling fncs::publish for topic '" << topic << "' value '" << tosend << "'");
+      NS_LOG_INFO ("FncsApplication::HandleRead: calling fncs::publish for topic '" << topic << "' value '" << v2send << "'");
      
-     
+      fncs::publish("finish", v2send);
     }
-    fncs::publish("finish", tosend);
 }
 
 } // Namespace ns3
